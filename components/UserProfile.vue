1b4977f163b224d9dd49876aa2bc3b9d76242e1e
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-09-21 18:48:05 +0300

    alignment issues fixed + styling

--- components/UserProfile.vue
+++ components/UserProfile.vue
@@ -2,8 +2,11 @@
   <view class="profile">
 <!--    Have to figure out how to get user profile picture from API-->
     <Image :source="{uri: 'https://www.irishrsa.ie/wp-content/uploads/2017/03/default-avatar.png'}"
-           :style="{width: 75, height: 75, borderRadius: 50}" />
+           :style="{width: 75, height: 75, borderRadius: 50}" class="profilePicture" />
     <text class="primaryText">{{username}}</text>
+    <touchable-opacity :on-press="uploadProfilePicture" class="profilePictureButton">
+      <text class="profilePictureButtonText">Change profile picture</text>
+    </touchable-opacity>
     <scroll-view ref="pagePosts">
       <UserPost v-for="post in posts" :key="post.id" :userPostText="post.title" :id="post.id" :dimensions="post.dimensions"
                 :files="post.files" :username="post.username" :profilePic="post.profilePic" :likes="post.likes" ></UserPost>
@@ -21,21 +24,42 @@ export default {
   },
   components:{
     UserPost
+  },
+  methods:{
+    uploadProfilePicture(){
+      // Have to figure out connection between profile picture and API
+      alert("New feature coming soon!")
+    }
   }
 }
 </script>
 
 <style>
+.profilePicture{
+  align-self: center;
+}
 .profile{
   flex: 1;
   align-self: center;
   margin-top: 5%;
 }
 .primaryText{
+  align-self: center;
   margin-top: 2%;
   margin-bottom: 5%;
   color: #AAAAAA;
   font-size: 20px;
   font-weight: 500;
+}
+.profilePictureButton{
+  align-self: center;
+  background-color: #505050;
+  width: 50%;
+}
+.profilePictureButtonText{
+  align-self: center;
+  font-size: 20px;
+  font-weight: 400;
+  color: ghostwhite;
 }
 </style>
