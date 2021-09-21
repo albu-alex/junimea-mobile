6022e747a759a64fae58bb0c1875bfd214b06e69
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-09-21 14:55:50 +0300

    created UserProfile.vue component

---
+++ components/UserProfile.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="profile">
+    <text>{{username}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  props:{
+    username: String
+  }
+}
+</script>
+
+<style>
+
+</style>
