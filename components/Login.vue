9bf99ba644ae07e4642d337e46c6c8adeb4a98bb
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-09-23 12:42:37 +0300

    loading shown when waiting for axios request

--- components/Login.vue
+++ components/Login.vue
@@ -3,14 +3,14 @@
 <template>
   <view class="userLogin">
     <Loading v-if="isLoading"/>
-<!--    <view v-if="isLoading" :style="{flex: 1, justifyContent: 'center'}">-->
-<!--      <activity-indicator size="large" color="dimgrey" />-->
-<!--    </view>-->
-    <text class="pageIntro" v-if="!isLoading">Welcome to juni.</text>
-    <text-input v-if="!isLoading" placeholderTextColor="ghostwhite" placeholder="Username or Email" v-model="username" class="textInput"></text-input>
-    <text-input v-if="!isLoading" placeholderTextColor="ghostwhite" placeholder="Email Address" v-model="email" class="textInput" v-if="!registeredUser"></text-input>
-    <text-input v-if="!isLoading" placeholderTextColor="ghostwhite" :secureTextEntry="!showPassword" placeholder="Password" v-model="password" class="textInput"></text-input>
-    <view v-if="!isLoading" class="loginButtons">
+    <view v-if="waitingForSubmit" :style="{flex: 1, justifyContent: 'center'}">
+      <activity-indicator size="large" color="dimgrey" />
+    </view>
+    <text class="pageIntro" v-if="!isLoading&&!waitingForSubmit">Welcome to juni.</text>
+    <text-input v-if="!isLoading&&!waitingForSubmit" placeholderTextColor="ghostwhite" placeholder="Username or Email" v-model="username" class="textInput"></text-input>
+    <text-input v-if="!isLoading&&!waitingForSubmit" placeholderTextColor="ghostwhite" placeholder="Email Address" v-model="email" class="textInput" v-if="!registeredUser"></text-input>
+    <text-input v-if="!isLoading&&!waitingForSubmit" placeholderTextColor="ghostwhite" :secureTextEntry="!showPassword" placeholder="Password" v-model="password" class="textInput"></text-input>
+    <view v-if="!isLoading&&!waitingForSubmit" class="loginButtons">
       <touchable-opacity class="loginButton" :on-press="verifyLogin">
         <text class="loginButtonText">Login</text>
       </touchable-opacity>
@@ -18,11 +18,11 @@
         <text class="loginButtonText">Register</text>
       </touchable-opacity>
     </view>
-    <touchable-opacity v-if="!isLoading" class="loginButton" :on-press="loginAsGuest">
+    <touchable-opacity v-if="!isLoading&&!waitingForSubmit" class="loginButton" :on-press="loginAsGuest">
       <text class="loginButtonText">Login as guest</text>
     </touchable-opacity>
 <!--    To be replaced with actual logo-->
-    <Image v-if="!isLoading" :source="{uri: 'https://i.makeagif.com/media/3-16-2016/_xIU5P.gif'}"
+    <Image v-if="!isLoading&&!waitingForSubmit" :source="{uri: 'https://i.makeagif.com/media/3-16-2016/_xIU5P.gif'}"
            :style="{width: pageWidth/2, height: (pageWidth/512)*512/2}" />
   </view>
 </template>
@@ -43,6 +43,7 @@ export default {
   },
   data(){
     return {
+      waitingForSubmit: false,
       //This variable is responsible for displaying the loading component
       isLoading: true,
       //These variables are used to dynamically resize the photo on any device
@@ -58,6 +59,7 @@ export default {
     }
   },
   methods:{
+    //This function sleeps the loading component, such that an actual loading is simulated
     sleep(ms){
       return new Promise(resolve => setTimeout(resolve, ms));
     },
@@ -69,6 +71,7 @@ export default {
       //This variable checks if the login operation was successful or not
       let loggedIn = false;
       let shownPassword = false;
+      this.waitingForSubmit = true;
       await axios({
         method: 'post',
         url: 'http://52.57.118.176/Auth/Login',
@@ -99,46 +102,49 @@ export default {
       //Data from the text-inputs is reset
       this.username = "";
       this.password = "";
+      this.waitingForSubmit = false;
       //Debugging purposes only
       // this.$emit('verifyLogin', "test");
     },
     //The registerNewUser method registers the user if he cannot be found in the database;
     //If the user is not registered, a text-input with an email address placeholder will appear
     async registerNewUser(){
-      if(this.registeredUser)
+      if(this.registeredUser) {
         this.registeredUser = false;
-      else{
-        let isRegistered = false;
-        await axios({
-          method: 'post',
-          url: 'http://52.57.118.176/Auth/Register',
-          data:{
-            "UserName": this.username,
-            "Email": this.email,
-            "Password": this.password
-          },
-          timeout: 4000
-        })
-        .then(function (response){
-          if(response.status === 200){
-            isRegistered = true;
-            alert("New user registered successfully!");
-          }
-        })
-        .catch(function (response){
-          if(response.status === 500){
-            alert("This account has already been registered!");
-          }
-          else
-            alert(response);
-        });
-        if(isRegistered)
-          this.$emit('verifyLogin', "test");
+        return;
       }
+      this.waitingForSubmit = true;
+      let isRegistered = false;
+      await axios({
+        method: 'post',
+        url: 'http://52.57.118.176/Auth/Register',
+        data:{
+          "UserName": this.username,
+          "Email": this.email,
+          "Password": this.password
+        },
+        timeout: 4000
+      })
+      .then(function (response){
+        if(response.status === 200){
+          isRegistered = true;
+          alert("New user registered successfully!");
+        }
+      })
+      .catch(function (response){
+        if(response.status === 500){
+          alert("This account has already been registered!");
+        }
+        else
+          alert(response);
+      });
+      if(isRegistered)
+        this.$emit('verifyLogin', "test");
       //Data from the text-inputs is reset
       this.username = "";
       this.email = "";
       this.password = "";
+      this.waitingForSubmit = false;
       // Debugging purposes only
       // this.$emit('verifyLogin', "test");
     },
