55cecfd47dc9ef99380c3fd92015be5c15fc4491
Alex <alexmhh1234@gmail.com>
2021-10-06 15:55:35 +0300

    Login.vue issue is caused by the image itself; need to change

--- components/Login.vue
+++ components/Login.vue
@@ -46,7 +46,7 @@
       </touchable-opacity>
     </view>
     <touchable-opacity :on-press="junimeaOnFacebook">
-      <Image v-if="!isLoading&&!waitingForSubmit" :source="require('../assets/unicorn-colorat-cu-scris.gif')"
+      <Image v-if="!isLoading&&!waitingForSubmit" :source="require('../assets/unicorn-colorat-cu-scris.png')"
              :style="{width: pageWidth/2, height: (pageWidth/1335)*1335/2}" />
     </touchable-opacity>
     <touchable-opacity v-if="!waitingForSubmit" :on-press="reportBugs">
