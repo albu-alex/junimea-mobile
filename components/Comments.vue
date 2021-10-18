966163e652c4525533ee5d5fc608f159bb037e07
Alex <alexmhh1234@gmail.com>
2021-10-18 18:37:14 +0300

    Comments component styling

--- components/Comments.vue
+++ components/Comments.vue
@@ -4,20 +4,44 @@
     <Image :source="{uri: 'https://www.irishrsa.ie/wp-content/uploads/2017/03/default-avatar.png'}"
            :style="{width: 20, height:20, borderRadius: 50, marginRight: 5}" />
     <text class="headerTextDark">user</text>
-    <Image :source="require('../assets/three-dots.png')"
-           :style="{width: 25, height:10, marginTop: 5}" class="reportComment" />
+    <touchable-opacity :on-press="report">
+      <Image :source="require('../assets/three-dots.png')"
+             :style="{width: 20, height:10, marginTop: 5}" class="reportComment" />
+    </touchable-opacity>
   </view>
-  <text class="primaryTextDark">comment</text>
+  <text :style="{marginLeft: 5}" class="primaryTextDark">comment</text>
+  <view class="commentInteraction">
+    <touchable-opacity :style="{marginLeft: 5, marginRight: 50}" :activeOpacity="1" :on-press="likeComment">
+      <text class="secondaryTextDark">Like</text>
+    </touchable-opacity>
+    <touchable-opacity :activeOpacity="1" :on-press="replyToComment">
+      <text class="secondaryTextDark">Reply</text>
+    </touchable-opacity>
+  </view>
+  <text-input v-model="commentText" class="addNewCommentDark" placeholder="Reply..." placeholderTextColor="dimgrey"
+              :multiline="true" keyboardAppearance="dark" :style="{borderRadius: 10, paddingHorizontal: 7, marginLeft:5}"/>
 </view>
 <view class="commentsLight" v-else>
   <view class="commentHeader">
     <Image :source="{uri: 'https://www.irishrsa.ie/wp-content/uploads/2017/03/default-avatar.png'}"
            :style="{width: 20, height:20, borderRadius: 50, marginRight: 5}" />
     <text class="headerTextLight">user</text>
-    <Image :source="require('../assets/three-dots.png')"
-           :style="{width: 25, height:10, marginTop: 5}" class="reportComment" />
+    <touchable-opacity :on-press="report">
+      <Image :source="require('../assets/three-dots.png')"
+             :style="{width: 20, height:10, marginTop: 5}" class="reportComment" />
+    </touchable-opacity>
   </view>
-  <text class="primaryTextLight">comment</text>
+  <text :style="{marginLeft: 5}" class="primaryTextLight">comment</text>
+  <view class="commentInteraction">
+    <touchable-opacity :style="{marginLeft: 5, marginRight: 50}" :activeOpacity="1" :on-press="likeComment">
+      <text class="secondaryTextLight">Like</text>
+    </touchable-opacity>
+    <touchable-opacity :activeOpacity="1" :on-press="replyToComment">
+      <text class="secondaryTextLight">Reply</text>
+    </touchable-opacity>
+  </view>
+  <text-input v-model="commentText" class="addNewCommentLight" placeholder="Reply..."
+              :multiline="true" keyboardAppearance="light" :style="{borderRadius: 10, paddingHorizontal: 7, marginLeft: 5}" />
 </view>
 </template>
 
@@ -27,20 +51,57 @@ export default {
   props:{
     isDarkTheme: Boolean,
   },
+  data(){
+    return{
+      commentText: ""
+    }
+  },
   async beforeMount(){
     await this.loadComments();
   },
   methods:{
     async loadComments(){
       //This post will load the comments of the post when API will be deployed
+    },
+    report(){
+      alert("Not yet available");
+    },
+    likeComment(){
+      alert("Like not available yet")
+    },
+    replyToComment(){
+      alert("Reply not available yet")
     }
   }
 }
 </script>
 
 <style>
+.commentInteraction{
+  flex-direction: row;
+  justify-content: flex-start;
+  margin-bottom: 4%;
+}
+.addNewCommentLight{
+  align-self: flex-start;
+  background-color: #DADADA;
+  color: #969696;
+  width: 60%;
+  height: 25px;
+  font-weight: 500;
+  font-size: 14px;
+}
+.addNewCommentDark{
+  align-self: flex-start;
+  background-color: #252525;
+  color: dimgrey;
+  width: 60%;
+  height: 25px;
+  font-weight: 500;
+  font-size: 14px;
+}
 .reportComment{
-  margin-left: 75%;
+  margin-left: 85%;
 }
 .commentsDark{
   background-color: #151515;
@@ -68,16 +129,21 @@ export default {
   justify-content: flex-start;
   flex-direction: row;
 }
+.secondaryTextDark{
+  color: #AAAAAA;
+  font-size: 12px;
+  font-weight: 600;
+}
 .primaryTextDark{
   color: #AAAAAA;
   font-size: 16px;
-  font-weight: 400;
-  margin-bottom: 1%;
+  font-weight: 200;
+  margin-bottom: 2%;
 }
 .primaryTextLight{
   color: #555555;
   font-size: 16px;
-  font-weight: 400;
-  margin-bottom: 1%;
+  font-weight: 200;
+  margin-bottom: 2%;
 }
 </style>
