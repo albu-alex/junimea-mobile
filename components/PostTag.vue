87f8dad023f884f78586a3edafeefaa16df574ad
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-12-04 17:52:48 +0200

    created a new PostTag component

---
+++ components/PostTag.vue
@@ -0,0 +1,37 @@
+<template>
+<view class="darkTag" v-if="isDarkTheme">
+  <text class="darkText">{{tag}}</text>
+</view>
+<view class="lightTag" v-else>
+  <text class="lightText">{{tag}}</text>
+</view>
+</template>
+
+<script>
+export default {
+  name: "PostTag",
+  props:{
+    isDarkTheme: Boolean,
+    tag: String
+  }
+}
+</script>
+
+<style>
+.darkTag{
+  background-color: #151515;
+  border-width: 1px;
+  border-color: #AAAAAA;
+}
+.darkText{
+  color: #AAAAAA;
+}
+.lightTag{
+  background-color: #EAEAEA;
+  border-width: 1px;
+  border-color: #555555;
+}
+.lightText{
+  color: #555555;
+}
+</style>
