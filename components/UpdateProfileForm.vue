ebe9f8347b6204683bf72bf5a2262603ead09150
Albu Alex-Stefan <alexmhh1234@gmail.com>
2022-01-20 17:20:41 +0200

    Basic UpdateProfileForm styling

--- components/UpdateProfileForm.vue
+++ components/UpdateProfileForm.vue
@@ -1,36 +1,40 @@
 <template>
 <view :class="{ updateProfileDark: (isDarkTheme), updateProfileLight: (!isDarkTheme)}">
-  <text-input v-if="isDarkTheme" :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="ghostwhite" placeholder="First Name"
-              v-model="firstName" class="inputFormDark">
-  </text-input>
-  <text-input v-else :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="#070000" placeholder="First Name"
-              v-model="firstName" class="inputFormLight">
-  </text-input>
-  <text-input v-if="isDarkTheme" :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="ghostwhite" placeholder="Last Name"
-              v-model="lastName" class="inputFormDark">
-  </text-input>
-  <text-input v-else :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="#070000" placeholder="Last Name"
-              v-model="lastName" class="inputFormLight">
-  </text-input>
-  <text-input v-if="isDarkTheme" :autoCorrect="false" keyboardType="email-address" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="ghostwhite" placeholder="Email address"
-              v-model="emailAddress" class="inputFormDark">
-  </text-input>
-  <text-input v-else :autoCorrect="false" keyboardType="email-address" :style="{borderRadius: 10, paddingHorizontal: 2}"
-              placeholderTextColor="#070000" placeholder="Email address"
-              v-model="emailAddress" class="inputFormLight">
-  </text-input>
-  <touchable-opacity :on-press="updateProfile">
-    <text :class="{ sendButtonTextDark: (isDarkTheme), sendButtonTextLight: (!isDarkTheme)}">Send</text>
-  </touchable-opacity>
+  <OwnStatusBar :isDarkTheme="isDarkTheme" :style="{zIndex: 2}"/>
+  <view :style="{alignSelf: 'center', marginTop: '45%', flex: 1}">
+    <text-input v-if="isDarkTheme" :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="ghostwhite" placeholder="First Name"
+                v-model="firstName" class="inputFormDark">
+    </text-input>
+    <text-input v-else :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="#070000" placeholder="First Name"
+                v-model="firstName" class="inputFormLight">
+    </text-input>
+    <text-input v-if="isDarkTheme" :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="ghostwhite" placeholder="Last Name"
+                v-model="lastName" class="inputFormDark">
+    </text-input>
+    <text-input v-else :autoCorrect="false" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="#070000" placeholder="Last Name"
+                v-model="lastName" class="inputFormLight">
+    </text-input>
+    <text-input v-if="isDarkTheme" :autoCorrect="false" keyboardType="email-address" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="ghostwhite" placeholder="Email address"
+                v-model="emailAddress" class="inputFormDark">
+    </text-input>
+    <text-input v-else :autoCorrect="false" keyboardType="email-address" :style="{borderRadius: 10, paddingHorizontal: 5}"
+                placeholderTextColor="#070000" placeholder="Email address"
+                v-model="emailAddress" class="inputFormLight">
+    </text-input>
+    <touchable-opacity :on-press="updateProfile" :style="{marginTop: '25%'}">
+      <text :class="{ sendButtonTextDark: (isDarkTheme), sendButtonTextLight: (!isDarkTheme)}">Send</text>
+    </touchable-opacity>
+  </view>
 </view>
 </template>
 
 <script>
+import OwnStatusBar from "./StatusBar";
 import axios from 'axios'
 import {Alert} from "react-native";
 export default {
@@ -47,6 +51,9 @@ export default {
     navigation:{
       type: Object
     }
+  },
+  components:{
+    OwnStatusBar
   },
   methods: {
     async updateProfile() {
@@ -111,36 +118,38 @@ export default {
 <style>
 .updateProfileDark{
   background-color: #252525;
-  align-items: center;
-  justify-content: center;
   flex: 1;
 }
 .updateProfileLight{
   background-color: #DADADA;
-  align-items: center;
-  justify-content: center;
   flex: 1;
 }
 .inputFormDark{
   background-color: #505050;
-  width: 90%;
-  margin-bottom: 1%;
+  width: 325px;
+  height: 50px;
+  margin-bottom: 3%;
   color: ghostwhite;
   font-weight: 200;
 }
 .inputFormLight{
   background-color: #AFAFAF;
-  width: 90%;
-  margin-bottom: 1%;
+  width: 325px;
+  height: 50px;
+  margin-bottom: 3%;
   color: ghostwhite;
   font-weight: 200;
 }
 .sendButtonTextDark{
   color: #AAAAAA;
-  font-weight: 200;
+  font-weight: 600;
+  font-size: 20px;
+  align-self: center;
 }
 .sendButtonTextLight{
   color: #555555;
-  font-weight: 200;
+  font-weight: 600;
+  font-size: 20px;
+  align-self: center;
 }
 </style>
