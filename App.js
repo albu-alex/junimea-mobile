084c104f083c346ba05f491a6163abd818649eca
Albu Alex-Stefan <alexmhh1234@gmail.com>
2022-02-02 23:21:04 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  const [text, setText] = useState("")
+  useEffect(() => {
+    setText("React Native")
+  });
+
+    return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
