79e999f6092d2d1d32c911397c98b0483cf5a1e2
alexm <alexmhh1234@gmail.com>
2022-02-15 10:34:41 +0200

    Finish Search component styling

--- components/Search.js
+++ components/Search.js
@@ -3,18 +3,37 @@ import { Text, View, TouchableOpacity, ScrollView,
     TextInput, Platform, TouchableWithoutFeedback, Keyboard, RefreshControl } from 'react-native';
 import React, {useState, useCallback, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 //css stylesheet import
 import { styles } from "../styles/SearchStyles";
 
 
 export default function Search({ navigation }){
+    const [searchText, setSearchText] = useState("");
     return(
         <View style={styles.container}>
             <StatusBar style="auto"/>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={styles.inner}>
-                    <Text>Search!</Text>
+                    <View style={styles.header}>
+                        <Icon style={styles.searchIcon} name='search' size={24} color={'#070700'}/>
+                        <TextInput autoCorrect={false} placeholderTextColor={'#070700'} placeholder={'Search Junimea'}
+                                   onChangeText={newSearchText => setSearchText(newSearchText)}
+                                   defaultValue={searchText} style={styles.textInput} />
+                        <TouchableOpacity style={{borderRadius: 10}} onPress={() => null}>
+                            <Text style={styles.buttonText}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <Text style={styles.primaryText}>Trending</Text>
+                    <ScrollView>
+                        <TouchableOpacity onPress={() => null}>
+                            <Text style={styles.tags}>Steaua</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.tags}>Dinamo</Text>
+                        <Text style={styles.tags}>CFR Cluj</Text>
+                        <Text style={styles.tags}>Astra</Text>
+                    </ScrollView>
                 </View>
             </TouchableWithoutFeedback>
         </View>
--- styles/SearchStyles.js
+++ styles/SearchStyles.js
@@ -10,6 +10,51 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#DADADA"
     },
+    header:{
+        marginTop: '5%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    buttonText:{
+        fontSize: 20,
+        fontWeight: '500',
+        marginTop: '10%',
+        marginRight: '3%',
+        marginLeft: '6%',
+        color: "#e34234"
+    },
+    primaryText:{
+        color: "#555555",
+        fontSize: 24,
+        fontWeight: "700",
+        marginLeft: "2%",
+        marginTop: "10%"
+    },
+    tags:{
+        color: "#555555",
+        fontSize: 20,
+        fontWeight: "400",
+        marginLeft: "5%",
+        marginTop: "2%"
+    },
+    textInput:{
+        backgroundColor: "#AFAFAF",
+        width: "75%",
+        height: "100%",
+        marginBottom: "5%",
+        marginLeft: "-6%",
+        color: "#070700",
+        paddingTop: 10,
+        borderRadius: 10,
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+    searchIcon:{
+        zIndex: 2,
+        paddingTop: 10,
+        paddingLeft: 15,
+        marginLeft: "2%"
+    }
 });
 
 export {styles}
