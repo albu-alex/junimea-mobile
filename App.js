26c2e910110b0fcf09c22e833edb2f0bd1616567
alexmhh <alexmhh1234@gmail.com>
2022-03-19 18:08:10 +0200

    Add change theme support to app

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 //npm import
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, LightTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AppearanceProvider, useColorScheme } from "react-native-appearance";
 
 //custom components import
 import Login from "./components/Login";
@@ -10,12 +11,15 @@ import Main from "./components/Main";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const scheme = useColorScheme()
     return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="Main" component={Main} />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <AppearanceProvider>
+          <NavigationContainer theme={scheme === 'dark' ? DarkTheme : LightTheme}>
+            <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+              <Stack.Screen name="Login" component={Login}/>
+              <Stack.Screen name="Main" component={Main} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </AppearanceProvider>
   );
 }
