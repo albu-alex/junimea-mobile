2f9827650ff3e6968d416a0d6b8a25241fc2c5c9
Albu Alex-Stefan <alexmhh1234@gmail.com>
2022-03-30 11:23:54 +0300

    Add full light/dark mode support for Search

--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,12 @@
 //npm import
 import { Text, View, TouchableOpacity, ScrollView,
     TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
+//custom method import
+import { getScheme } from "../methods/App/getScheme";
 
 //css stylesheet import
 import { styles } from "../styles/SearchStyles";
@@ -11,28 +14,48 @@ import { styles } from "../styles/SearchStyles";
 
 export default function Search({ navigation }){
     const [searchText, setSearchText] = useState("");
+    const [scheme, setScheme] = useState("")
+    const [backgroundColor, setBackgroundColor] = useState("")
+    const [inputBackgroundColor, setInputBackgroundColor] = useState("")
+    const [textColor, setTextColor] = useState("")
+    const [iconColor, setIconColor] = useState("")
+    useEffect(async () => {
+        setScheme(await getScheme())
+        setBackgroundColor((scheme === 'dark') ? '#252525' : '#DADADA')
+        setInputBackgroundColor((scheme === 'dark') ? '#AFAFAF' : '#505050')
+        setTextColor((scheme === "dark") ? '#AAAAAA' : '#555555')
+        setIconColor((scheme === 'dark') ? '#070700' : "ghostwhite")
+    });
     return(
-        <View style={styles.container}>
+        <View style={[styles.container,
+            {backgroundColor: backgroundColor}]}>
             <StatusBar style="auto"/>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                <View style={styles.inner}>
+                <View style={[styles.inner,
+                    {backgroundColor: backgroundColor}]}>
                     <View style={styles.header}>
-                        <Icon style={styles.searchIcon} name='search' size={24} color={'#070700'}/>
-                        <TextInput autoCorrect={false} placeholderTextColor={'#070700'} placeholder={'Search Junimea'}
+                        <Icon style={styles.searchIcon} name='search' size={24} color={iconColor}/>
+                        <TextInput autoCorrect={false} placeholderTextColor={iconColor} placeholder={'Search Junimea'}
                                    onChangeText={newSearchText => setSearchText(newSearchText)} multiline={true}
-                                   defaultValue={searchText} style={styles.textInput} />
+                                   defaultValue={searchText} style={[styles.textInput,
+                            {backgroundColor: inputBackgroundColor, color: iconColor}]} />
                         <TouchableOpacity style={{borderRadius: 10}} onPress={() => navigation.navigate("MainPage")}>
                             <Text style={styles.buttonText}>Cancel</Text>
                         </TouchableOpacity>
                     </View>
-                    <Text style={styles.primaryText}>Trending</Text>
+                    <Text style={[styles.primaryText,
+                        {color: textColor}]}>Trending</Text>
                     <ScrollView>
                         <TouchableOpacity onPress={() => null} activeOpacity={0.8}>
-                            <Text style={styles.tags}>Steaua</Text>
+                            <Text style={[styles.tags,
+                                {color: textColor}]}>Steaua</Text>
                         </TouchableOpacity>
-                        <Text style={styles.tags}>Dinamo</Text>
-                        <Text style={styles.tags}>CFR Cluj</Text>
-                        <Text style={styles.tags}>Astra</Text>
+                        <Text style={[styles.tags,
+                            {color: textColor}]}>Dinamo</Text>
+                        <Text style={[styles.tags,
+                            {color: textColor}]}>CFR Cluj</Text>
+                        <Text style={[styles.tags,
+                            {color: textColor}]}>Astra</Text>
                     </ScrollView>
                 </View>
             </TouchableWithoutFeedback>
--- styles/SearchStyles.js
+++ styles/SearchStyles.js
@@ -3,12 +3,10 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#DADADA'
     },
     inner: {
         paddingTop: '15%',
         flex: 1,
-        backgroundColor: "#DADADA"
     },
     header:{
         marginTop: '5%',
@@ -24,26 +22,22 @@ const styles = StyleSheet.create({
         color: "#e34234"
     },
     primaryText:{
-        color: "#555555",
         fontSize: 24,
         fontWeight: "700",
         marginLeft: "2%",
         marginTop: "10%"
     },
     tags:{
-        color: "#555555",
         fontSize: 20,
         fontWeight: "400",
         marginLeft: "5%",
         marginTop: "2%"
     },
     textInput:{
-        backgroundColor: "#AFAFAF",
         width: "75%",
         height: "100%",
         marginBottom: "5%",
         marginLeft: "-6%",
-        color: "#070700",
         paddingTop: 10,
         borderRadius: 10,
         paddingLeft: 25,
