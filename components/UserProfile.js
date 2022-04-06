f63fa02bc13ae179d2e0aec0b4faae84abe8f142
alexmhh <alexmhh1234@gmail.com>
2022-04-06 16:33:58 +0300

    UserProfile FlatList optimization + starting to adapt UserProfile to light/dark mode

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -13,46 +13,58 @@ import { getUserDetails } from "../methods/UserProfile/getUserDetails"
 import { getPosts } from "../methods/UserProfile/getPosts"
 import { uploadProfilePicture } from "../methods/UserProfile/uploadProfilePicture"
 import { uploadFile } from "../methods/UserProfile/uploadFile";
+import { getScheme } from "../methods/App/getScheme";
 
 //custom components import
 import UserPost from "./UserPost"
 
 export default function UserProfile({ navigation }){
-    const [profilePicture, setProfilePicture] = useState('');
-    const [username, setUsername] = useState('');
-    const [userID, setUserID] = useState('');
-    const [data, setData] = useState([]);
-    const [refreshing, setRefreshing] = useState(false);
+    const [profilePicture, setProfilePicture] = useState('')
+    const [username, setUsername] = useState('')
+    const [userID, setUserID] = useState('')
+    const [data, setData] = useState([])
+    const [refreshing, setRefreshing] = useState(false)
     const [postNumber, setPostNumber] = useState(200)
+    const [backgroundColor, setBackgroundColor] = useState("")
+    const [iconColor, setIconColor] = useState("")
+
+    const scheme = getScheme()
+
     useEffect(async () => {
         const [newProfilePicture, newUsermane, newUserID] = await getUserDetails();
         setProfilePicture(newProfilePicture)
         setUsername(newUsermane)
         setUserID(newUserID)
 
+        setBackgroundColor((scheme === 'dark') ? '#252525' : '#DADADA')
+        setIconColor((scheme === 'dark') ? '#AFAFAF': '#505050')
+
         await fetchPosts()
-    });
+    })
+
     const changeProfilePicture = async() => {
         await uploadFile()
     }
+
     const fetchPosts = async() => {
         let posts = await getPosts()
         setData(posts)
     }
+
     const renderItem = ({ item }) => (
         (userID === item.userId) ?
             <UserPost key={item.id} id={item.id} />
             : null
-    );
+    )
     return(
-        <View style={styles.container}>
+        <View style={[styles.container, {backgroundColor: backgroundColor}]}>
             <StatusBar style='auto' />
-            <View style={styles.header}>
+            <View style={[styles.header, {backgroundColor: backgroundColor}]}>
                 <TouchableOpacity onPress={() => navigation.navigate("MainPage")}>
-                    <Icon name='arrow-left' size={30} color={"#505050"} />
+                    <Icon name='arrow-left' size={30} color={iconColor} />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => alert("report user")}>
-                    <Icon name='ellipsis-h' size={30} color={'#505050'} />
+                    <Icon name='ellipsis-h' size={30} color={iconColor} />
                 </TouchableOpacity>
             </View>
             <View>
@@ -80,10 +92,13 @@ export default function UserProfile({ navigation }){
                 </TouchableOpacity>
             </View>
             <FlatList
-                windowSize={20}
+                windowSize={60}
+                initialNumToRender={10}
                 removeClippedSubviews={true}
                 data={data}
+                extraData={this.state}
                 renderItem={renderItem}
+                onEndReachedThreshold={0.5}
                 refreshControl={
                     <RefreshControl
                         refreshing={refreshing}
--- styles/UserProfileStyles.js
+++ styles/UserProfileStyles.js
@@ -3,14 +3,12 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#DADADA",
         paddingTop: '15%'
     },
     header:{
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        backgroundColor: '#DADADA',
         marginTop: '2%',
         paddingLeft: '5%',
         paddingRight: '5%'
